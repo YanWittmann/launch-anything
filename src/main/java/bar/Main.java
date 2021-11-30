90e257e02bd7eb30f783d6ce493d5b70a69f2dc7
Yan Wittmann <thomas3654william@gmail.com>
2021-11-30 20:16:32 +0100

    Set version to 2.2-SNAPSHOT

--- src/main/java/bar/Main.java
+++ src/main/java/bar/Main.java
@@ -28,7 +28,7 @@ import static java.awt.Desktop.getDesktop;
 
 public class Main {
 
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "2.2-SNAPSHOT";
 
     public static void main(String[] args) {
         Util.registerFont("font/Comfortaa-Regular.ttf");
