89fcc63762ff766d7a380f2a13d84b1f8e72e816
Skyball2000 <thomas3654william@gmail.com>
2022-04-20 18:39:59 +0200

    Added Euler Phi method to math evaluator

--- launch-anything-application/src/main/java/bar/util/Util.java
+++ launch-anything-application/src/main/java/bar/util/Util.java
@@ -336,6 +336,28 @@ public abstract class Util {
                 return createGgtFunction(args);
             }
         });
+        directFunctions.add(new Function("phi", 1) {
+            @Override
+            public double apply(double... args) {
+                // https://stackoverflow.com/questions/13216853/calculating-eulers-totient-function-for-very-large-numbers-java
+                int n = (int) args[0];  // this is the number you want to find the totient of
+                int tot = n; // this will be the totient at the end of the sample
+                for (int p = 2; p * p <= n; p++) {
+                    if (n % p == 0) {
+                        tot /= p;
+                        tot *= (p - 1);
+                        while (n % p == 0)
+                            n /= p;
+                    }
+                }
+                if (n > 1) { // now n is the largest prime divisor
+                    tot /= n;
+                    tot *= (n - 1);
+                }
+
+                return tot;
+            }
+        });
     }
 
     private static int createQuersummeFunction(double[] args) {
